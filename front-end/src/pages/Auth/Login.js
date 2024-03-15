516b07bc5e90057c189e1cd5d42ffc7a82b26202
ruchi-kr <ruchi.kumari63@gmail.com>
2024-03-16 00:29:14 +0530

    third and final commit

--- front-end/src/pages/Auth/Login.js
+++ front-end/src/pages/Auth/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
       }
     } catch (error) {
       console.log(error);
-      toast.error("Something went wrong");
+      // toast.error("Something went wrong");
     }
   };
 
